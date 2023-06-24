3eddcdce4df30b306f030d835d88d0b9f14b908b
Po-Cheng Yeh <blp100x@hotmail.com>
2023-06-24 12:58:02 +0800

    feat: setting up colors and fonts

--- components/Header.js
+++ components/Header.js
@@ -1,7 +1,13 @@
-import { Heading } from "@chakra-ui/react"
+import { Box, Heading } from "@chakra-ui/react";
 
-const Header = () =>{
-return <Heading></Heading>
-}
+const Header = () => {
+  return (
+    <Box textAlign="center" pt={["32px", "80px", "48px"]}>
+      <Heading fontFamily="Kumbh_Sans" textColor="#D7E0FF" fontSize={32}>
+        pomodoro
+      </Heading>
+    </Box>
+  );
+};
 
-export default Header
+export default Header;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
 import { Heading, Text } from "@chakra-ui/react";
+import Header from "../components/Header";
 
 const Page = () => {
   return (
-    <Text fontFamily="Roboto_Slab" textStyle="h1">
-      Hello, World.
-    </Text>
+    <>
+      <Header />
+      <Text fontFamily="Roboto_Slab" textStyle="h1" as="h1" textAlign="center">
+        Hello, World.
+      </Text>
+    </>
   );
 };
 
